83b3202fbb08a92d2d35781218fb911029382bf7
Wellington Oliveira <well.oliveira.snt@gmail.com>
2021-04-25 23:06:38 -0300

    Increment network size

--- bnoc/scripts/submit.sh
+++ bnoc/scripts/submit.sh
@@ -5,7 +5,7 @@ rm -rf simg_images bnoc.simg
 cp simg_images/bnoc bnoc.simg
 rclone sync cloud:/hpc/containers/simg_images /workspace/bnoc/simg_images
 
-for i in 10 11 12
+for i in 6 7 8 9 10 11 12
 do
   ./srun_submit.sh "$i" > "srun_submit_$i.sh"
   sbatch -v "srun_submit_$i.sh"
