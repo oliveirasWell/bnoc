5e456400d582461eeae9af12d601b397b956da1f
Wellington Oliveira <well.oliveira.snt@gmail.com>
2021-04-25 23:19:11 -0300

    Send message when ci build finished

--- .github/workflows/send.sh
+++ .github/workflows/send.sh
@@ -15,3 +15,5 @@ echo "${RCLONE_CONF}" | base64 -d >> "${RCLONE_FILE}"
 echo "Enviando arquivos..."
 
 rclone copyto "bnoc.simg" "cloud:hpc/containers/${COLLECTION_CONTAINER}/bnoc.simg"
+
+curl -s -X POST -H 'Content-Type: application/json' -d "{\"chat_id\": \"$CHATID\", \"text\": \"Build Finished\", \"disable_notification\": false}"  "https://api.telegram.org/bot$BOTKEY/sendMessage"
