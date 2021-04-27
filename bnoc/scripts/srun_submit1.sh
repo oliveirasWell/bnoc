e89b4506291ffcbc49a1a1099888c7cb8e03e625
Well Oliveira <well.oliveira.snt@gmail.com>
2021-04-27 11:13:58 -0300

    Update the srun_submit1.sh with default values

---
+++ bnoc/scripts/srun_submit1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH -J bnoc_$1 # Job name
+#SBATCH -o %j.out # Name of stdout output file (%j expands to jobId)
+#SBATCH -N 1 # Total number of nodes requested
+#SBATCH -t 03:00:00
+#SBATCH --partition=fast
+#SBATCH --mail-user=wellington.oliveira@estudante.ufscar.br
+#SBATCH --mail-type=ALL
+#SBATCH --mem=341500MB
+#SBATCH --account=usuario
+
+. send_notification.sh
+
+echo "Setup Environment..."
+
+module purge
+module load singularity
+
+sendOutputFile() {
+  cp "${SLURM_JOB_ID}.out" "${SLURM_JOB_ID}.txt"
+  sendFile "${SLURM_JOB_ID}.txt"
+}
+
+sendMsg "Job ${SLURM_JOB_ID} starting"
+
+echo "Starting..."
+
+srun --mpi=pmix_v2 singularity run --bind=/var/spool/slurm:/var/spool/slurm bnoc.simg /opt/conda/envs/mfbn/bin/python /opt/bnoc.py -cnf "/opt/bipartite-time-ncol.json"
+retCode=$?
+echo "$retCode"
+if [[ "$retCode" -ne 0 ]]; then
+  echo "An error accrued"
+  sendErr
+  sendOutputFile
+  exit 1
+fi
+echo "Finished!"
+
+sendOutputFile
+sendFile "output/bipartite-time-ncol-inf.json"
+tar -cvf  "${SLURM_JOB_ID}.tar" "output"
+rclone copy "${SLURM_JOB_ID}.tar" "cloud:hpc/containers/outputs/"
+
+sendMsg "Job ${SLURM_JOB_ID} finished :D"
--- bnoc/scripts/submit.sh
+++ bnoc/scripts/submit.sh
@@ -5,6 +5,8 @@ rm -rf simg_images bnoc.simg output*
 rclone sync cloud:/hpc/containers/simg_images simg_images
 cp simg_images/bnoc.simg bnoc.simg
 
+sbatch -v srun_submit1.sh
+
 for i in 6 7 8 9 10 11 12
 do
   ./srun_submit.sh "$i" > "srun_submit_$i.sh"
