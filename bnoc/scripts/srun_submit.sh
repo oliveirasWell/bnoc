e7914b9fe3b97f663f18778f7596e81514c53939
Wellington Oliveira <well.oliveira.snt@gmail.com>
2021-04-30 15:20:08 -0300

    New memory and (maybe) networks

--- bnoc/scripts/srun_submit.sh
+++ bnoc/scripts/srun_submit.sh
@@ -8,7 +8,7 @@ echo "#!/bin/bash
 #SBATCH --partition=fast
 #SBATCH --mail-user=wellington.oliveira@estudante.ufscar.br
 #SBATCH --mail-type=ALL
-#SBATCH --mem=356500MB
+#SBATCH --mem=354700MB
 #SBATCH --account=usuario
 
 . send_notification.sh
