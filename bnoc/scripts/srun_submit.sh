413e65726cfa5bceecf05b13f6e730d647b5c4f7
Wellington Oliveira <well.oliveira.snt@gmail.com>
2021-04-30 06:16:53 -0300

    New memory sizes, less jobs

--- bnoc/scripts/srun_submit.sh
+++ bnoc/scripts/srun_submit.sh
@@ -3,12 +3,12 @@
 echo "#!/bin/bash
 #SBATCH -J bnoc_$1 # Job name
 #SBATCH -o %j.out # Name of stdout output file (%j expands to jobId)
-#SBATCH -N 2 # Total number of nodes requested
+#SBATCH -N 1 # Total number of nodes requested
 #SBATCH -t 03:00:00
 #SBATCH --partition=fast
 #SBATCH --mail-user=wellington.oliveira@estudante.ufscar.br
 #SBATCH --mail-type=ALL
-#SBATCH --mem=351500MB
+#SBATCH --mem=352500MB
 #SBATCH --account=usuario
 
 . send_notification.sh
