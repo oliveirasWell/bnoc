276543bfb9f93b678323fac13fe26c42467ac92a
Wellington Oliveira <well.oliveira.snt@gmail.com>
2021-05-05 11:39:27 -0300

    Change limit time

--- bnoc/scripts/srun_submit12.sh
+++ bnoc/scripts/srun_submit12.sh
@@ -2,7 +2,7 @@
 #SBATCH -J bnoc_12 # Job name
 #SBATCH -o %j.out # Name of stdout output file (%j expands to jobId)
 #SBATCH -N 1 # Total number of nodes requested
-#SBATCH -t 03:00:00
+#SBATCH -t 06:00:00
 #SBATCH --partition=normal
 #SBATCH --mail-user=wellington.oliveira@estudante.ufscar.br
 #SBATCH --mail-type=ALL
